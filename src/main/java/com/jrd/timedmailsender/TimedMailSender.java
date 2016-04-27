155fbbfa4408fe43296a4ee4e11ff8ecb78cba1f
Jakub Radlak <jradlak@gmail.com>
2016-04-27 19:54:20 +0200

    First steps in project

---
+++ src/main/java/com/jrd/timedmailsender/TimedMailSender.java
@@ -0,0 +1,10 @@
+package com.jrd.timedmailsender;
+
+/**
+ * Created by jakub on 27.04.16.
+ */
+public class TimedMailSender {
+    public static void main(String[] args) {
+        System.out.println("Hello TimedMailSender!!");
+    }
+}
